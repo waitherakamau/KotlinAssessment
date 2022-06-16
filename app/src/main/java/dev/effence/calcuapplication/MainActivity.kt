d6440b59210292e9f078033659f4774f92d69328
waitherakamau <effencekamau@gmail.com>
2022-06-16 11:05:45 +0300

    validate

--- app/src/main/java/dev/effence/calcuapplication/MainActivity.kt
+++ app/src/main/java/dev/effence/calcuapplication/MainActivity.kt
@@ -8,9 +8,10 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var btnAdd:Button
     lateinit var btnSubtract:Button
-    lateinit var btnMultiply:Button
+    lateinit var btnDivide:Button
     lateinit var btnModulus:Button
     lateinit var tilNum1:TextInputLayout
     lateinit var tilNum2:TextInputLayout
@@ -22,56 +23,56 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btnAdd=findViewById(R.id.btnAdd)
         btnSubtract=findViewById(R.id.btnSubtract)
-        btnMultiply=findViewById(R.id.btnMultiply)
+        btnDivide=findViewById(R.id. btnDivide)
         btnModulus=findViewById(R.id.btnModulus)
-        tilNum1=findViewById(R.id.tilnum1)
-        etnum2=findViewById(R.id.etNum1)
-        tilNum2=findViewById(R.id.tilnum2)
+        tilNum1=findViewById(R.id.tilNum1)
+        etnum1=findViewById(R.id.etNum1)
+        tilNum2=findViewById(R.id.tilNum2)
         etnum2=findViewById(R.id.etNum2)
         tvResults=findViewById(R.id.tvResults)
 
         btnAdd.setOnClickListener {
-            var num1=etnum1.text.toString().toInt()
-            var num2=etnum2.text.toString().toInt()
-            addition(num1, num2)
-//            startActivity(intent)
-//            validatetext()
+            var num1=etnum1.text.toString().toDouble()
+            var num2=etnum2.text.toString().toDouble()
+           addition(num1,num2)
+
+            validateText()
         }
         btnSubtract.setOnClickListener {
-            var num1=etnum1.text.toString().toInt()
-            var num2=etnum2.text.toString().toInt()
+            var num1=etnum1.text.toString().toDouble()
+            var num2=etnum2.text.toString().toDouble()
             subtraction( num1,num2)
         }
-        btnMultiply.setOnClickListener {
-            var num1=etnum1.text.toString().toInt()
-            var num2=etnum2.text.toString().toInt()
-            multiplication(num1, num2)
+        btnDivide.setOnClickListener {
+            var num1=etnum1.text.toString().toDouble()
+            var num2=etnum2.text.toString().toDouble()
+            division(num1, num2)
         }
         btnModulus.setOnClickListener {
-            var num1=etnum1.text.toString().toInt()
-            var num2=etnum2.text.toString().toInt()
+            var num1=etnum1.text.toString().toDouble()
+            var num2=etnum2.text.toString().toDouble()
             modulus(num1, num2)
         }
 
     }
-    fun addition(num1:Int,num2:Int){
+    fun addition(num1: Double, num2: Double){
         var add=num1+num2
         tvResults.text=add.toString()
     }
 
-    fun subtraction(num1: Int,num2: Int){
+    fun subtraction(num1: Double, num2: Double){
         var subtract=num1-num2
         tvResults.text=subtract.toString()
     }
-    fun multiplication(num1: Int,num2: Int){
-        var multiply=num1*num2
-        tvResults.text=multiply.toString()
+    fun division(num1: Double, num2: Double){
+        var divide =num1 /num2
+        tvResults.text=divide.toString()
     }
-    fun modulus(num1: Int,num2: Int){
+    fun modulus(num1: Double, num2: Double){
         var modulus=num1%num2
         tvResults.text=modulus.toString()
     }
-    fun validatetext(){
+    fun validateText(){
        var num1=etnum1.text.toString()
         var num2=etnum2.text.toString()
 
